5004ca7708e544ae80e66b17bad4004ed10e002c
Arany Medve <zsolt.aranyi@gmail.com>
2024-08-30 13:29:33 +0200

    teljes csere

--- fedorasetup.sh
+++ fedorasetup.sh
@@ -9,25 +9,24 @@ sudo hostnamectl set-hostname "beartp"
 
 sudo update-crypto-policies --set DEFAULT:FEDORA32
 
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc -y
+sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+sudo dnf config-manager --set-enabled fedora-cisco-openh264
+
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update -y 
+
+
 sudo dnf update -y
 sudo dnf upgrade -y
-sudo dnf install calibre dnf-utils vlc chrome-gnome-shell -y
-sudo dnf groupupdate sound-and-video
-sudo dnf install -y libdvdcss
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel lame\* --exclude=lame-devel -y
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted -y
-sudo dnf install \*-firmware gnome-tweaks gnome-extensions-app gnome-tweak-tool 'google-roboto*' 'mozilla-fira*' fira-code-fonts -y
-sudo dnf install tlp tlp-rdw steam remmina mc @development-tools kernel-headers kernel-devel dkms bridge-utils libvirt virt-install qemu-kvm -y
-sudo dnf config-manager --set-enabled fedora-cisco-openh264
-sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
-sudo dnf install -y NetworkManager-openconnect-gnome NetworkManager-openvpn-gnome NetworkManager-pptp-gnome NetworkManager-openconnect-gnome NetworkManager-l2tp-gnome
-sudo dnf install -y dconf-editor redshift fastfetch 
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install calibre dnf-utils vlc chrome-gnome-shell libdvdcss gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel lame\* --exclude=lame-devel rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted \*-firmware gnome-tweaks gnome-extensions-app gnome-tweak-tool 'google-roboto*' 'mozilla-fira*' fira-code-fonts tlp tlp-rdw steam remmina mc @development-tools kernel-headers kernel-devel dkms bridge-utils libvirt virt-install qemu-kvm gstreamer1-plugin-openh264 mozilla-openh264 NetworkManager-openconnect-gnome NetworkManager-openvpn-gnome NetworkManager-pptp-gnome NetworkManager-openconnect-gnome NetworkManager-l2tp-gnome dconf-editor redshift fastfetch code microsoft-edge-stable -y
+
+# sudo dnf groupupdate sound-and-video 
 
 sudo dnf group upgrade --with-optional Multimedia core --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin sound-and-video -y
 
-sudo fwupdmgr refresh --force
-sudo fwupdmgr get-updates
-sudo fwupdmgr update
+#flatpak stuff
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub org.gnome.Extensions
@@ -47,19 +46,14 @@ flatpak install -y com.adobe.Reader
 flatpak install -y flathub io.freetubeapp.FreeTube
 flatpak install -y flathub com.spotify.Client
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc -y
-sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update -y 
-sudo dnf install code -y
-sudo dnf install microsoft-edge-stable -y
+# vegyes tennivalók
 
-#sudo dnf -y install qemu-kvm libvirt virt-install cirt-manager
-#sudo dnf -y install edk2-ovmf swtpm swtpm-tools 
-#sudo systemctl enable --now libvirtd
 sudo gsettings set org.gnome.desktop.interface show-battery-percentage true
 sudo systemctl disable NetworkManager-wait-online.service
 sudo rm /etc/xdg/autostart/org.gnome.Software.desktop
+sudo fwupdmgr refresh --force
+sudo fwupdmgr get-updates
+sudo fwupdmgr update
 
 
 # Telepítendő extension-ök
@@ -74,6 +68,3 @@ sudo rm /etc/xdg/autostart/org.gnome.Software.desktop
 # https://extensions.gnome.org/extension/6655/openweather/
 # https://extensions.gnome.org/extension/3088/extension-list/
 # https://extensions.gnome.org/extension/5489/search-light/
-
-
-
