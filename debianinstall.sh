8bef2015bfad7c0a060fea57bfb8570b035df8ee
Arany Medve <zsolt.aranyi@gmail.com>
2025-07-23 12:11:32 +0200

    Add files via upload

--- debianinstall.sh
+++ debianinstall.sh
@@ -46,3 +46,40 @@ flatpak install -y com.emqx.MQTTX
 flatpak install -y com.adobe.Reader
 #flatpak install -y io.freetubeapp.FreeTube
 flatpak install -y com.spotify.Client
+
+sudo apt install -y gnome-terminal
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+wget -O /home/zsolt/Letöltések/docker-desktop-amd64.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
+sudo apt-get update
+sudo apt-get install -y /home/zsolt/Letöltések/docker-desktop-amd64.deb
+systemctl --user start docker-desktop
+
+cat << 'EOA' | tee /home/zsolt/gpginput.txt
+%echo Generating a default key
+Key-Type: RSA
+Key-Length: 2048
+Subkey-Type: RSA
+Subkey-Length: 2048
+Name-Real: Zsolt Aranyi
+Name-Email: zsolt.aranyi@gmail.com
+Expire-Date: 0
+Passphrase: Medve63383!
+%commit
+%echo done
+EOA
+gpg --batch --generate-key /home/zsolt/gpginput.txt
+pass init $(gpg --list-secret-keys --keyid-format=long --with-colons | awk -F: '/^sec/ {print $5}')
+
