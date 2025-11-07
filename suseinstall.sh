87bef458231102e4ff196e696985708e63f3c973
Zsolt Aranyi <aranymedve@gmail.com>
2025-11-07 15:41:46 +0100

    mód

--- suseinstall.sh
+++ suseinstall.sh
@@ -6,13 +6,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" |sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
 
 sudo zypper dup
-sudo zypper --non-interactive install wget gpg git code \
-  mc remmina git \
-  make wget build-essential gpg vlc \
-  sudo NetworkManager-openvpn NetworkManager-l2tp \
-  ca-certificates curl flatpak flameshot libSDL2-2_0-0 \
-  steam-installer steam-devices  \
-  inotify-tools ca-certificates
+sudo zypper --non-interactive install wget gpg git code mc remmina git make wget gpg vlc sudo NetworkManager-openvpn NetworkManager-l2tp ca-certificates curl flatpak flameshot libSDL2-2_0-0 steam steam-devices inotify-tools ca-certificates
 
 
 git config --global user.name "Zsolt Aranyi"
