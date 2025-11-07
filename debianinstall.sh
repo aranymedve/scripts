300a2c15e74f12a17d0d99724979ebeae3218ff1
Zsolt Aranyi <aranymedve@gmail.com>
2025-11-07 17:21:27 +0100

    brave

--- debianinstall.sh
+++ debianinstall.sh
@@ -28,6 +28,11 @@ sudo apt install -y netselect-apt mc remmina git \
   inotify-tools
 
 
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+sudo apt update
+sudo apt install brave-browser -y
+
 git config --global user.name "Zsolt Aranyi"
 git config --global user.email aranymedve@gmail.com
 
