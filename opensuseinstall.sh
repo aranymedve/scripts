2b075ba47ce969699c51f326356ca5726ba64eee
Zsolt Aranyi <aranymedve@gmail.com>
2026-02-02 00:39:15 +0100

    opensuse cuccok

--- opensuseinstall.sh
+++ opensuseinstall.sh
@@ -12,7 +12,7 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 sudo zypper dup
 sudo zypper --non-interactive install wget gpg git code mc remmina git make wget gpg vlc sudo NetworkManager-openvpn NetworkManager-l2tp ca-certificates curl flatpak flameshot libSDL2-2_0-0 steam steam-devices inotify-tools ca-certificates plasma-nm5-l2tp strongswan strongswan-ipsec
 
-sudo zypper in --force NetworkManager-l2tp plasma-nm5-l2tp strongswan
+sudo zypper in --force NetworkManager-l2tp plasma-nm5-l2tp strongswan NetworkManager-strongswan
 
 sudo systemctl enable --now strongswan
 
@@ -22,24 +22,34 @@ git config --global user.email aranymedve@gmail.com
 mkdir ~/.local/share/remmina
 cp remmina/* ~/.local/share/remmina/
 
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install -y --user com.viber.Viber
-sudo flatpak install -y com.synology.SynologyDrive
-sudo flatpak install -y com.synology.SynologyAssistant
-sudo flatpak install -y com.heroicgameslauncher.hgl
-sudo flatpak install -y com.anydesk.Anydesk
-sudo flatpak install -y com.emqx.MQTTX
-sudo flatpak install -y com.adobe.Reader
-sudo flatpak install -y com.spotify.Client
-sudo flatpak install -y org.telegram.desktop
-sudo flatpak install -y com.github.IsmaelMartinez.teams_for_linux
-sudo flatpak install -y com.freerdp.FreeRDP
-sudo flatpak install -y it.mijorus.gearlever
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-#sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+flatpak install -y com.viber.Viber com.synology.SynologyDrive com.synology.SynologyAssistant \
+  com.heroicgameslauncher.hgl com.anydesk.Anydesk com.emqx.MQTTX com.adobe.Reader \
+  com.spotify.Client org.telegram.desktop com.github.IsmaelMartinez.teams_for_linux \
+  com.freerdp.FreeRDP it.mijorus.gearlever com.github.iwalton3.jellyfin-media-player || true
 
-#sudo zypper install brave-browser
 
 wget -O /home/zsolt/Letöltések/UpNote.AppImage https://download.getupnote.com/app/UpNote.AppImage
 
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+
+# Enable the repository
+sudo tee /etc/zypp/repos.d/firefoxpwa.repo > /dev/null <<EOF
+[firefoxpwa]
+name=FirefoxPWA
+type=rpm-md
+baseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch
+gpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey
+       https://packagecloud.io/filips/FirefoxPWA/gpgkey/filips-FirefoxPWA-912AD9BE47FEB404.pub.gpg
+repo_gpgcheck=1
+pkg_gpgcheck=1
+autorefresh=1
+enabled=1
+EOF
+
+# Import GPG key and update Zypper cache
+sudo zypper --gpg-auto-import-keys refresh firefoxpwa
+
+# Install the package
+sudo zypper install firefoxpwa
